d96ddb717b8f079021b183b16943fa938d3f2940
PatriotRossii <patriotrossii2019@mail.ru>
2021-12-08 20:11:38 +0500

    Minor fixes

--- get_bdates.py
+++ get_bdates.py
@@ -1,6 +1,13 @@
 import time
 import vk
-from typing import Set
+from typing import List
+
+class User:
+	def __init__(self, id, first_name, last_name, bdate):
+		self.id = id
+		self.first_name = first_name
+		self.last_name = last_name
+		self.bdate = bdate
 
 
 def chunks(lst, n):
@@ -8,7 +15,7 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
-def get_bdates(api: vk.API, members: Set[int], delay: float = 0.4):
+def get_bdates(api: vk.API, members: List[int], delay: float = 0.4):
 	birth_dates = []
 	for chunk in chunks(members, 1000):
 		user_ids = ",".join([str(e) for e in chunk])
@@ -16,7 +23,7 @@ def get_bdates(api: vk.API, members: Set[int], delay: float = 0.4):
 
 		for user in response:
 			if "bdate" in user:
-				birth_dates.append(user["bdate"])
+				birth_dates.append(User(user["id"], user["first_name"], user["last_name"], user["bdate"]))
 		time.sleep(delay)
-		
+
 	return birth_dates
--- get_members.py
+++ get_members.py
@@ -9,12 +9,12 @@ def chunks(lst, n):
 def get_members(api: vk.API, group_id: int, fields: str = "", delay: float = 0.4):
 	spy_requests = api.groups.getMembers(group_id=group_id, fields=fields)
 	count = spy_requests["count"]
-	members = set(spy_requests["items"])
+	members = set([item["id"] for item in spy_requests["items"]])
 
 	if count > 1000:
 		for i in range(1, (count // 1000) + 1):
 			time.sleep(delay)
 			members.update(
-				set(api.groups.getMembers(group_id=group_id, fields=fields, offset=i*1000)["items"])
+				set([item["id"] for item in api.groups.getMembers(group_id=group_id, fields=fields, offset=i*1000)["items"]])
 			)
 	return list(members)
